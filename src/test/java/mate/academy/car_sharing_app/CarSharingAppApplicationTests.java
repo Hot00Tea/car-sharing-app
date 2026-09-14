5b72ab434bd08bb640dde1dcda76402ffb01c389
Hot00Tea <cha9n.1488@gmail.com>
2026-09-14 15:54:22 +0300

    Use Testcontainers for integration tests

--- src/test/java/mate/academy/car_sharing_app/CarSharingAppApplicationTests.java
+++ src/test/java/mate/academy/car_sharing_app/CarSharingAppApplicationTests.java
@@ -2,14 +2,21 @@ package mate.academy.car_sharing_app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 
-@ActiveProfiles("test")
+@Testcontainers
 @SpringBootTest
 class CarSharingAppApplicationTests {
+
+	@Container
+	@ServiceConnection
+	static MySQLContainer<?> mysql =
+			new MySQLContainer<>("mysql:8.4");
 
 	@Test
 	void contextLoads() {
 	}
-
 }
